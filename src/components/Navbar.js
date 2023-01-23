1d803ea6c7c8bff951da943b3b72a1914d6eae71
Edgars01 <miclex82@tutanota.com>
2023-01-23 12:18:59 +0200

    Router, Routes, Route

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
@@ -6,7 +7,8 @@ export const Navbar = () => {
         <nav className="navbar">
             <div className="navbar-container">
                 <Link to="/" className="navbar-logo">
-                  
+                  PCSTORE
+                  <i class="fa-solid fa-laptop"></i>
                 </Link>
             </div>
         </nav>
