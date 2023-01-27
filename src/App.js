864f879f58b3be87f4c1bbcd07171193458a12fb
Edgars01 <miclex82@tutanota.com>
2023-01-27 14:24:09 +0200

    remove bootsrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
+
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
 import Products from './components/pages/Products';
-import Carousel from './components/Carousel';
+
 
 function App() {
 
@@ -24,7 +25,7 @@ function App() {
 
 
         <Products />
-        <Carousel />
+
         <Footer />
 
       </Router>
