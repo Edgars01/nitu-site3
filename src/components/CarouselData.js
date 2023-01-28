dee4acf592a203865b4bcdd07cbd3a48dbcb61db
Edgars01 <miclex82@tutanota.com>
2023-01-28 21:09:59 +0200

    idk..

--- src/components/CarouselData.js
+++ src/components/CarouselData.js
@@ -1,15 +1,20 @@
+import image1 from './pages/images/1img.jpg';
+import image2 from './pages/images/2img.jpeg';
+import image3 from './pages/images/3img.jpg';
+import image4 from './pages/images/4img.webp';
+
 export const CarouselData = [
     {
-      image: '../pages/images/1img.jpg'
+      image: image1
     },
     {
-        image: '../components/pages/images/2img.jpeg'
+      image: image2
     },
     {
-        image: '../components/pages/images/3img.jpg'
+      image: image3
     },
     {
-        image: '../components/pages/images/4img.webp'
+      image: image4
     },
     
   ];
